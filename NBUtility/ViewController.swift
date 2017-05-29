fbe5c7019156a40a91eb50aae3bee94432082cae
Fahid Attique <fahidattique55@gmail.com>
2017-05-29 12:48:24 +0500

    Initial Commit

---
+++ NBUtility/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBUtility
+//
+//  Created by Fahid Attique on 5/29/17.
+//  Copyright © 2017 NextBridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NBUtilityTests/NBUtilityTests.swift
@@ -0,0 +1,36 @@
+//
+//  NBUtilityTests.swift
+//  NBUtilityTests
+//
+//  Created by Fahid Attique on 5/29/17.
+//  Copyright © 2017 NextBridge. All rights reserved.
+//
+
+import XCTest
+@testable import NBUtility
+
+class NBUtilityTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
